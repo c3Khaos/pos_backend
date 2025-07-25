243bc5e0f731dc0a9d69db0544f723dfb2424395
christopherchege <chrisshege35@gmail.com>
2025-07-25 18:23:45 +0300

    added hashed db characters

--- models.py
+++ models.py
@@ -10,7 +10,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(80), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(256), nullable=False)
     role = db.Column(db.String(50), default='user', nullable=False)
 
 
