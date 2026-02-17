9d0e391807c46688fec9d81ee9d7e75724f384ab
christopherchege <chrisshege35@gmail.com>
2026-02-18 01:19:34 +0300

    chore: add a tempoorary admin seed user for initial login

---
+++ seed_admin.py
@@ -0,0 +1,14 @@
+from app import app, db
+from models import User
+from werkzeug.security import generate_password_hash
+
+with app.app_context():
+    if not User.query.filter_by(username="admin").first():
+        admin = User(
+            username="admin",
+            email="chrisshege35@gmail.com",
+            password_hash=generate_password_hash("chris")  # hashed, not plaintext
+        )
+        db.session.add(admin)
+        db.session.commit()
+        print("Admin user created")
