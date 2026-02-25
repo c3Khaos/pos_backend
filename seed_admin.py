e109c83aae5d048e1b0dadcb0c3c029a2aa6ebba
christopherchege <chrisshege35@gmail.com>
2026-02-25 22:56:35 +0300

    fix: update mpesa callback URL to production Render endpoint

--- seed_admin.py
+++ seed_admin.py
@@ -6,12 +6,11 @@ from models import User
 from werkzeug.security import generate_password_hash
 
 with app.app_context():
-    if not User.query.filter_by(username="admin").first():
-        admin = User(
-            username="admin",
-            email="chrisshege35@gmail.com",
-            password_hash=generate_password_hash("christof")  # hashed, not plaintext
+        user1 = User(
+            username="chege",
+            email="dev.chris@gmail.com",
+            password_hash=generate_password_hash("chris")  # hashed, not plaintext
         )
-        db.session.add(admin)
+        db.session.add(user1)
         db.session.commit()
-        print("Admin user created")
+        print("User created")
